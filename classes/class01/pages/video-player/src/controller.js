092a2c2c4fb10cb75bcfad6abce949ca381727e0
rodrigorvsn <rodrigovictor81@gmail.com>
2023-02-27 22:08:03 -0300

    feat: play and pause video with eye blink

---
+++ classes/class01/pages/video-player/src/controller.js
@@ -0,0 +1,66 @@
+export default class Controller {
+  #view
+  #camera
+  #worker
+  #blinkCounter = 0
+
+  constructor({ view, camera, worker }) {
+    this.#view = view
+    this.#camera = camera
+    this.#worker = this.#configureWorker(worker)
+
+    this.#view.configureOnBtnClick(this.onBtnStart.bind(this))
+  }
+
+  #configureWorker(worker) {
+    let ready = false
+    worker.onmessage = ({ data }) => {
+      if (data === 'READY') {
+        this.#view.enableButton()
+        ready = true
+        return
+      }
+
+      const blinked = data.blinked
+      this.#blinkCounter += blinked
+      this.#view.togglePlayVideo()
+    }
+
+    return {
+      send(message) {
+        if (!ready) return
+        worker.postMessage(message)
+      }
+    }
+  }
+
+  static async initialize(deps) {
+    const controller = new Controller(deps)
+    controller.log('not detecting eye blink yet')
+    return controller.init()
+  }
+
+  async init() {
+    console.log('init')
+  }
+
+  loop() {
+    const video = this.#camera.video
+    const image = this.#view.getVideoFrame(video)
+    this.#worker.send(image)
+    this.log('detecting eye blink')
+
+    setTimeout(() => this.loop(), 100)
+  }
+
+  log(text) {
+    const times = ` - blinked times: ${this.#blinkCounter}`
+    this.#view.log(`status ${text}`.concat(this.#blinkCounter ? times : ''))
+  }
+
+  onBtnStart() {
+    this.log('initializing detection')
+    this.#blinkCounter = 0
+    this.loop()
+  }
+}
---
+++ classes/class01/pages/video-player/src/factory.js
@@ -0,0 +1,56 @@
+import Camera from "../../../lib/shared/camera.js"
+import { supportsWorkType } from "../../../lib/shared/util.js"
+import Controller from "./controller.js"
+import Service from "./service.js"
+import View from "./view.js"
+
+async function getWorker() {
+  if (supportsWorkType()) {
+    const worker = new Worker('./src/worker.js', { type: 'module' })
+    return worker
+  }
+
+  console.warn("Your browser doesn't support esm modules on webworkers")
+
+  await import("https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js")
+  await import("https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js")
+  await import("https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js")
+  await import("https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js")
+
+  const service = new Service({
+    faceLandmarksDetection: window.faceLandmarksDetection
+  })
+
+  const workerMock = {
+    async postMessage(video) {
+      const blinked = await service.handBlinked(video)
+      if (!blinked) return
+
+      workerMock.onmessage({ data: { blinked } })
+    },
+    onmessage(msg) { }
+  }
+
+  await service.loadModel()
+
+  setTimeout(() => worker.onmessage({ data: 'READY' }), 1800);
+
+  return workerMock
+}
+
+const worker = await getWorker()
+
+const camera = await Camera.init()
+const [rootPath] = window.location.href.split('/pages/')
+
+const factory = {
+  async initalize() {
+    return Controller.initialize({
+      view: new View(),
+      camera,
+      worker,
+    })
+  }
+}
+
+export default factory
